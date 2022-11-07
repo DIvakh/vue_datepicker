fd60dce7d9b3e247362aa79d0ad7b93fc8e6a174
“Dmitry Ivakh” <divakh@atncorp.com>
2022-11-08 00:03:05 +0200

    Error in config file

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true,
-  filenameHashing: false,
-  productionSourceMap: false,
-  configureWebpack: {
-    output: {
-      filename: 'js/datepick.js',
-      chunkFilename: 'js/datepick_chunk.js'
-    }
-  }
+  transpileDependencies: true
 });
